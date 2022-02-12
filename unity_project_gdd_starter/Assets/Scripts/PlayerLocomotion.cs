c6bf9a77e1cf5d419fb273ca6404603b825c2843
Untouchez <65210494+Untouchez@users.noreply.github.com>
2022-02-11 20:11:41 -0800

    lesson_0

---
+++ unity_project_gdd_starter/Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    public Animator anim;
+
+    public float acceleration;
+    public float decceleration;
+
+    public Vector3 rawInput;
+    public Vector3 calculatedInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleInputs();
+        anim.SetFloat("Vertical", calculatedInput.z);
+        anim.SetFloat("Horizontal", calculatedInput.x);
+    }
+
+    //CALCULATES AND SETS RAW INPUT AND CALCULATED INPUT
+    //CHANGES ANIMATOR TO PLAY CORRECT ANIMATION
+    void HandleInputs()
+    {
+        //GETS WASD INPUT
+        // W = 1 Y
+        // S = -1 Y
+        // A = -1 X
+        // D = 1 X
+        rawInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        if (rawInput.magnitude != 0)
+            calculatedInput = new Vector3(Mathf.MoveTowards(calculatedInput.x, rawInput.x, acceleration * Time.deltaTime), 0, Mathf.MoveTowards(calculatedInput.z, rawInput.z, acceleration * Time.deltaTime));
+        else
+            calculatedInput = new Vector3(Mathf.MoveTowards(calculatedInput.x, 0, acceleration * Time.deltaTime), 0, Mathf.MoveTowards(calculatedInput.z, 0, decceleration * Time.deltaTime));
+
+        calculatedInput = Vector3.ClampMagnitude(calculatedInput, 1.5f);
+    }
+}
---
+++ unity_project_gdd_starter/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    public bool canRotate;
+    public float turnSpeed;
+    PlayerLocomotion PL;
+    Camera mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PL = GetComponent<PlayerLocomotion>();
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleRotation();
+    }
+
+    void HandleRotation()
+    {
+        if (!canRotate)
+            return;
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(transform.TransformDirection(calculatedInput)), turnSpeed * Time.fixedDeltaTime);
+
+        if (PL.rawInput.magnitude != 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+}
