3d3187fdae86a382e0503e22bb1a4c0a2c1d6c09
Untouchez <65210494+Untouchez@users.noreply.github.com>
2022-03-03 15:51:35 -0800

    LESSON_0

---
+++ unity_project_gdd_starter/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+            Attack();
+    }
+
+    void Attack()
+    {
+        //WE WANT THIS FUNCTION TO ATTACK (PLAY AN ANIMATION, SEE IF HITS ANYTHING)
+
+        //1. find animation on mixamo we like for attack
+        //2. import into unity, update the controller,
+        //2.1 add it to player controller,
+        //2.2 set up transitions from anystate exit,
+        //2.3 add condition
+        //we want this function to call the trigger
+
+    }
+}
