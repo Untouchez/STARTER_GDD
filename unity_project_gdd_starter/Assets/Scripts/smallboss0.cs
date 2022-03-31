7ef778a05560e5c3148e12808aeb415938021d51
Alston Chan <alstonchan64@berkeley.edu>
2022-03-30 23:20:43 -0700

    added sword to small boss

--- unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
+++ unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
@@ -10,17 +10,22 @@ public class smallboss0 : MonoBehaviour
     public Transform player;
 
     public float attackRadius;
+
+    private Weapon weapon;
+
     // Start is called before the first frame update
     void Start()
     {
-    
+        weapon = GetComponentInChildren<Weapon>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Vector3.Distance(transform.position, player.position) <= attackRadius) {
+            //weapon.Enable();
             anim.SetTrigger("attack");
+            //StopCoroutine(weapon.turnOff());
         } else {
             agent.SetDestination(player.position);
         }
