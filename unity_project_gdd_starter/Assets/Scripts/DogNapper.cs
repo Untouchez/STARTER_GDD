287680ea00e222d713ae04bea84ceba00920af70
alexbottt <100449325+alexbottt@users.noreply.github.com>
2022-04-03 14:46:45 -0700

    added alex scene and dognapper prefab

--- unity_project_gdd_starter/Assets/Scripts/DogNapper.cs
+++ unity_project_gdd_starter/Assets/Scripts/DogNapper.cs
@@ -16,7 +16,7 @@ public class DogNapper : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
