42eb9e067c5b1af3d93636e61a9a6fcb75f47da7
Alston Chan <alstonchan64@berkeley.edu>
2022-04-05 12:48:17 -0700

    Added health component to Small_Boss prefab

--- unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
+++ unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
@@ -14,7 +14,7 @@ public class smallboss0 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+       player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
