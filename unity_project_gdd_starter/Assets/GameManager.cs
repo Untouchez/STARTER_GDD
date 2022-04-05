81146db90efdfcf96b777b1f8ccf666618b92e99
Alston Chan <alstonchan64@berkeley.edu>
2022-04-05 13:59:04 -0700

    Created StartScene

---
+++ unity_project_gdd_starter/Assets/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+
+    public string gameScene;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameScene);
+    }
+
+
+}
