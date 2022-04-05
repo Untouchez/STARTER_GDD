203f6a752a86778dd4b8b536329260a2aa1d8d07
Untouchez <65210494+Untouchez@users.noreply.github.com>
2022-04-05 14:48:14 -0700

    small boss edits and added berkeley level

--- unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
+++ unity_project_gdd_starter/Assets/Scripts/smallboss0.cs
@@ -5,14 +5,18 @@ using UnityEngine.AI;
 
 public class smallboss0 : Health
 {
+    [Space(10)]
     public Animator anim;
     public NavMeshAgent agent;
     public Transform player;
     public Weapon sword;
     public EnemyHealthBar healthBarPrefab;
     private EnemyHealthBar _healthBar;
+    private float lastAttack;
+    public float attackRate;
+    public bool isAttacking;
+    public float attackRange;
 
-    public float attackRadius;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,15 +29,50 @@ public class smallboss0 : Health
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.position) <= attackRadius) {
-            anim.SetTrigger("attack");
-        } else {
-            agent.SetDestination(player.position);
+        if (Time.time - lastAttack > 1 / attackRate)
+        {
+            if (!isAttacking)
+            {
+                //MAKE DECISION
+                lastAttack = Time.time;
+
+                if (DistanceToPlayer() <= attackRange)
+                {
+                    transform.LookAt(player);
+                    anim.SetTrigger("attack");
+                    agent.isStopped = true;
+                }
+                else
+                {
+                    agent.SetDestination(player.position);
+                    agent.isStopped = false;
+                }
+            }
         }
+
+        if (IsPlaying("attack"))
+        {
+            agent.isStopped = true;
+            isAttacking = true;
+        }
+        else
+            isAttacking = false;
+
+        anim.SetFloat("speed", agent.velocity.magnitude / agent.speed);
     }
     public void Hit(int damage)
     {
         sword.damage = damage;
         sword.Enable();
+    }
+
+    public bool IsPlaying(string clipName)
+    {
+        return this.anim.GetCurrentAnimatorStateInfo(0).IsName(clipName);
+    }
+
+    public float DistanceToPlayer()
+    {
+        return Vector3.Distance(transform.position, player.position);
     }
 }
