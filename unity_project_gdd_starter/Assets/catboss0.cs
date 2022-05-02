5a2bc9ea5101e19fb636bfd88fd82adf2cc9f4b2
hynicolecheng <hy.nicolecheng@berkeley.edu>
2022-05-02 15:12:35 -0700

    catboss test

---
+++ unity_project_gdd_starter/Assets/catboss0.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catboss0 : Health
+{
+    // Start is called before the first frame update
+
+    public float rotateSpeed;
+    public Player player;
+    private float lastAttack;
+    public float attackRate;
+    public bool isAttacking;
+    public float attackRange;
+    public float lastfired;
+    public float FireRate;
+
+    public Transform projectile;
+    
+
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform.GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAtPlayer();
+        if (Time.time - lastfired > 1 / FireRate)
+         {
+             Instantiate(projectile, transform.position, transform.rotation);
+             lastfired = Time.time;
+        }
+    }
+
+    public void LookAtPlayer()
+    {
+
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = player.transform.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = rotateSpeed * Time.deltaTime;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    
+    }
+
+    // public bool IsPlaying(string clipName)
+    // {
+    //     return this.anim.GetCurrentAnimatorStateInfo(0).IsName(clipName);
+    // }
+
+    public float DistanceToPlayer()
+    {
+        return Vector3.Distance(transform.position, player.transform.position);
+    }
+}
+
---
+++ unity_project_gdd_starter/Assets/catbossprojectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catbossprojectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float projectileSpeed;
+
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    public Player player;
+    
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
